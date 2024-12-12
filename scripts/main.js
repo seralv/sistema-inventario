60f761efc4c3e5d8cfcf10323552fb5b7976a738
seralv <sergioalvaro.14@gmail.com>
2024-12-12 12:30:17 -0400

    Updating functions

--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,7 @@ function crearInventario() {
       precio: 8,
     },
   ];
+
   return {
     agregarProducto: (nombre, cantidad, precio) => {
       const newProduct = { nombre, cantidad, precio };
@@ -24,19 +25,71 @@ function crearInventario() {
       );
 
       if (productoExistente) {
-        productoExistente.cantidad++;
+        productoExistente.cantidad += cantidad;
+        console.log(`Producto actualizado: ${productoExistente.nombre}`);
         console.log("productos:", productos);
       } else {
         productos.push(newProduct);
-        console.log(
-          `Producto ${JASON.stringify(productoExistente.name)} agregado`,
-        );
+        console.log(`Producto agregado: ${newProduct.nombre}`);
         console.log("productos:", productos);
       }
+    },
+    eliminarProducto: (nombre) => {
+      productos = productos.filter((product) => product.nombre !== nombre);
+      console.log("productos:", productos);
+    },
+    actualizarPrecio: (nombre, nuevoPrecio) => {
+      const productoExistente = productos.find(
+        (producto) => producto.nombre === nombre,
+      );
+      if (productoExistente) {
+        productoExistente.precio = nuevoPrecio;
+        console.log(`Producto actualizado: ${productoExistente.nombre}`);
+        console.log("productos:", productos);
+      } else {
+        console.log(`El producto ${nombre} no existe.`);
+      }
+    },
+    consultarProducto: (nombre) => {
+      let USDollar = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+      const productoExistente = productos.find(
+        (producto) => producto.nombre === nombre,
+      );
+      if (productoExistente) {
+        console.log(
+          `${productoExistente.nombre} - Cantidad: ${productoExistente.cantidad}, Precio: ${USDollar.format(productoExistente.precio)}`,
+        );
+      } else {
+        console.log(`El producto ${nombre} no existe`);
+      }
+    },
+    mostrarInventario: () => {
+      let USDollar = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+      console.log("--- INVENTARIO ---");
+      productos.forEach((producto, index) => {
+        console.log(
+          `${index + 1}. ${producto.nombre} - Cantidad: ${producto.cantidad}, Precio: ${USDollar.format(producto.precio)}`,
+        );
+      });
+    },
+    calcularValorTotal: () => {
+      // let total
     },
   };
 }
 
 const miInventario = crearInventario();
 
-miInventario.agregarProducto("Sandias", 5, 3);
+miInventario.agregarProducto("Peras", 5, 3);
+miInventario.agregarProducto("Sandias", 3, 3);
+miInventario.eliminarProducto("Naranjas");
+miInventario.actualizarPrecio("Kiwis", 10);
+miInventario.actualizarPrecio("Manzanas", 10);
+miInventario.consultarProducto("Manzanas");
+miInventario.mostrarInventario();
